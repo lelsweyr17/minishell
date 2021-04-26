53028ff18a875aa37f2e23020990d0232aef1c65
Crabb Marsha <cmarsha@pr-j2.kzn.21-school.ru>
2021-04-26 14:34:52 +0300

    parser_v_02.4.1 commands list malfunction fixed and main returned

--- minishell.c
+++ minishell.c
@@ -65,86 +65,86 @@ int			main(int argc, char *argv[], char *envp[])
 			hist = hist->next;
 		// all.input = ft_strdup("");
 		write(1, "::: ", 4);
-// 		tputs(save_cursor, 1, ft_iputchar);
-// 		do
-// 		{
-// 			// if (!ft_strchr(buf, '\n'))
-// 				ft_memset(buf, 0, res);
-// 			// bf = buf;
-// 			res = read(0, buf, 100);
-// 			// buf[res] = '\0';
-// 			// bf = ft_strchr(buf, '\n')
-// 			// if (*bf == '\n')
-// 			// 	*bf = '\0';
-// 			// res = get_next_line(0, &buf);
-// 			if (!ft_strcmp(buf, "\e[A") && hist->prev)// && hist->prev) // "\e[A"
-// 			{
-// 				tputs(restore_cursor, 1, ft_iputchar);
-// 				tputs(tigetstr("ed"), 1, ft_iputchar);
-// //				if (hist->prev)
-// 					hist = hist->prev; // UP
-// 				write(1, hist->content, ft_strlen(hist->content));
-// 			}
-// 			else if (!ft_strcmp(buf, "\e[B") && hist->next) // "\e[B"
-// 			{
-// 				tputs(restore_cursor, 1, ft_iputchar);
-// 				tputs(tigetstr("ed"), 1, ft_iputchar);
-// 				hist = hist->next; // DOWN
-// 				write(1, hist->content, ft_strlen(hist->content));
-// 			}
-// 			else if (!ft_strcmp(buf, "\e[C"))
-// 			{
-// 				continue ;
-// 				// tputs(restore_cursor, 1, ft_iputchar);
-// 				// tputs(tigetstr("ed"), 1, ft_iputchar);
-// 			}
-// 			else if (!ft_strcmp(buf, "\e[D"))
-// 			{
-// 				continue ;
-// 				// tputs(restore_cursor, 1, ft_iputchar);
-// 				// tputs(tigetstr("ed"), 1, ft_iputchar);
-// 			}
-// 			else if (!strcmp(buf, "\177") && len > 0 && res > 0) // "\177" BACKSPACE
-// 			{
-// 				tputs(cursor_left, 1, ft_iputchar);
-// 				tputs(tigetstr("ed"), 1, ft_iputchar);
-// 				((char *)hist->content)[--len] = '\0';
-// 			}
-// 			else if (!ft_strcmp(buf, "\3"))
-// 			{
-// 				write(1, "^C\n", 4);
-// 				break ;
-// 			}
-// 			else if (!ft_strcmp(buf, "\34"))
-// 			{
-// 				write(1, "^\\Quit: 3\n", 10);
-// 			}
-// 			else if (ft_strcmp(buf, "\4") && strcmp(buf, "\177"))
-// 			{
-// 			    write(1, buf, res);
-// 				if (!hist->next && !copy)
-// 				{
-// 					copy = ft_strdup(hist->content);
-// 				}
-// 				// all.input = str_free(&all.input, ft_strjoin(all.input, buf));
-// 				hist->content = str_free((char **)&hist->content, ft_strjoin(hist->content, buf));
-// 				len = ft_strlen(hist->content);
-// 				if (!ft_strcmp(buf, "\n"))
-// 					((char *)hist->content)[len - 1] = '\0';
-// 			}
-// 			if (!ft_strcmp(buf, "\4"))
-// 			{
-// 				write(1, "exit", 4);
-// 				exit (0);
-// 			}
-// 		} while (ft_strcmp(buf, "\n") && ft_strcmp(buf, "\4"));
-// 		all.input = (char *)hist->content;
-// 		if (isempty(hist->content, 0))
-// 		{
-// 			write(1, "EMPTY\n", 6);
-// 			// ft_memset(all.input, 0, len);
-// 			ft_memset(hist->content, 0, len);
-// 		}
+		tputs(save_cursor, 1, ft_iputchar);
+		do
+		{
+			// if (!ft_strchr(buf, '\n'))
+				ft_memset(buf, 0, res);
+			// bf = buf;
+			res = read(0, buf, 100);
+			// buf[res] = '\0';
+			// bf = ft_strchr(buf, '\n')
+			// if (*bf == '\n')
+			// 	*bf = '\0';
+			// res = get_next_line(0, &buf);
+			if (!ft_strcmp(buf, "\e[A") && hist->prev)// && hist->prev) // "\e[A"
+			{
+				tputs(restore_cursor, 1, ft_iputchar);
+				tputs(tigetstr("ed"), 1, ft_iputchar);
+//				if (hist->prev)
+					hist = hist->prev; // UP
+				write(1, hist->content, ft_strlen(hist->content));
+			}
+			else if (!ft_strcmp(buf, "\e[B") && hist->next) // "\e[B"
+			{
+				tputs(restore_cursor, 1, ft_iputchar);
+				tputs(tigetstr("ed"), 1, ft_iputchar);
+				hist = hist->next; // DOWN
+				write(1, hist->content, ft_strlen(hist->content));
+			}
+			else if (!ft_strcmp(buf, "\e[C"))
+			{
+				continue ;
+				// tputs(restore_cursor, 1, ft_iputchar);
+				// tputs(tigetstr("ed"), 1, ft_iputchar);
+			}
+			else if (!ft_strcmp(buf, "\e[D"))
+			{
+				continue ;
+				// tputs(restore_cursor, 1, ft_iputchar);
+				// tputs(tigetstr("ed"), 1, ft_iputchar);
+			}
+			else if (!strcmp(buf, "\177") && len > 0 && res > 0) // "\177" BACKSPACE
+			{
+				tputs(cursor_left, 1, ft_iputchar);
+				tputs(tigetstr("ed"), 1, ft_iputchar);
+				((char *)hist->content)[--len] = '\0';
+			}
+			else if (!ft_strcmp(buf, "\3"))
+			{
+				write(1, "^C\n", 4);
+				break ;
+			}
+			else if (!ft_strcmp(buf, "\34"))
+			{
+				write(1, "^\\Quit: 3\n", 10);
+			}
+			else if (ft_strcmp(buf, "\4") && strcmp(buf, "\177"))
+			{
+			    write(1, buf, res);
+				if (!hist->next && !copy)
+				{
+					copy = ft_strdup(hist->content);
+				}
+				// all.input = str_free(&all.input, ft_strjoin(all.input, buf));
+				hist->content = str_free((char **)&hist->content, ft_strjoin(hist->content, buf));
+				len = ft_strlen(hist->content);
+				if (!ft_strcmp(buf, "\n"))
+					((char *)hist->content)[len - 1] = '\0';
+			}
+			if (!ft_strcmp(buf, "\4"))
+			{
+				write(1, "exit", 4);
+				exit (0);
+			}
+		} while (ft_strcmp(buf, "\n") && ft_strcmp(buf, "\4"));
+		all.input = (char *)hist->content;
+		if (isempty(hist->content, 0))
+		{
+			write(1, "EMPTY\n", 6);
+			// ft_memset(all.input, 0, len);
+			ft_memset(hist->content, 0, len);
+		}
 // 		write(1, "VHOD:_", 6);
 // 		write(1, all.input, ft_strlen(all.input));
 // 		write(1, "_\n", 2);
@@ -174,12 +174,12 @@ int			main(int argc, char *argv[], char *envp[])
 		// all.input = "echo $USER ;";
 		// all.input = "as ;  ;";
 		// all.input = "a > 1 b > 2 c";
-		all.input = "1;2;3;4;5;6";
+		// all.input = "1;2;3;4;5;6";
 		buf[0] = '\n';
 		if (all.input[0] != '\0' && !ft_strcmp(buf, "\n") && *all.input != '\n')
 			parser(&all);
 		pars_free(&all);
-		exit (0);
+		// exit (0);
 	}
 	write(1, "\n", 1);
 	return (0);
